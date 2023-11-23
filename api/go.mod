d27c2d84cad686685be3bd8443518d166a2f159c
Li 'Daniel' Xie <lxie@opentext.com>
2023-11-23 14:01:49 +0800

    add first go file to retrieve weather info

---
+++ api/go.mod
@@ -0,0 +1,3 @@
+module github.com/danielxx2005/MyWeather
+
+go 1.21.4
---
+++ api/retrieveweather.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+)
+
+const apiKey = "YOUR_API_KEY"
+const apiEndpoint = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+
+type WeatherResponse struct {
+	Days []struct {
+		Date          string `json:"datetimeStr"`
+		Temperature   float64
+		Precipitation float64
+		// Add more fields as needed
+	} `json:"days"`
+}
+
+func main() {
+	location := "New York, NY"
+	startDate := time.Now().Format("2006-01-01")
+	endDate := time.Now().Format("2006-01-03")
+
+	client := resty.New()
+
+	resp, err := client.R().
+		SetQueryParam("key", apiKey).
+		SetQueryParam("location", location).
+		SetQueryParam("startDate", startDate).
+		SetQueryParam("endDate", endDate).
+		SetHeader("Content-Type", "application/json").
+		Get(apiEndpoint)
+
+	if err != nil {
+		log.Fatalf("Error making request: %v", err)
+	}
+
+	if resp.StatusCode() != 200 {
+		log.Fatalf("Error: %v", resp)
+	}
+
+	var weatherData WeatherResponse
+	err = json.Unmarshal(resp.Body(), &weatherData)
+	if err != nil {
+		log.Fatalf("Error decoding JSON response: %v", err)
+	}
+
+	// Print weather data
+	fmt.Printf("Weather for %s from %s to %s:\n", location, startDate, endDate)
+	for _, day := range weatherData.Days {
+		fmt.Printf("Date: %s, Temperature: %.2f°C, Precipitation: %.2fmm\n", day.Date, day.Temperature, day.Precipitation)
+	}
+}
